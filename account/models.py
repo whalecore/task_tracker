8f6987976449d4f5acb952bdeb613aa72e38f37a
whalecore <komnatakita@gmail.com>
2020-12-23 18:16:38 +0300

    added task models

--- account/models.py
+++ account/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Task(models.Model):
+    TD = 'To Do'
+    INP = 'In Progress'
+    TEST = 'Testing'
+    DN = 'Done'
+    STATUSES = [
+        ('To Do', 'To Do'),
+        ('In Progress', 'In Progress'),
+        ('Testing', 'Testing'),
+        ('Done', 'Done'),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=30, choices=STATUSES, default=TD)
+    created = models.DateTimeField(auto_now_add=True)
+    redline = models.DateTimeField()
+    deadline = models.DateTimeField()
+    worker = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Subtask(Task, models.Model):
+    task = models.ForeignKey(Task, on_delete=models.PROTECT)
+
+
+class Column(models.Model):
+    pass
